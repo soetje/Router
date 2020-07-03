fc454e2d2d7571b2680fc2d2842dc4bf08fbadbc
soetje <imredesoet@hotmail.com>
2020-07-03 12:15:16 +0200

    added routing: homepage, about, movies

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import DiscoverMoviesPage from './pages/DiscoverMoviesPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        hello guys
-      </header>
+    <div className='App'>
+        <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/DiscoverMoviesPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DiscoverMoviesPage() {
+  return (
+    <div>this is the discover movies page</div>
+  )
+}
